621286fdceda72a49d50c7ddd7689a956c952da3
Ali bourara <bourara.ali.pro@gmail.com>
2023-06-23 18:27:51 +0100

    correction fuzzbuzz

--- Week1/Day 5/OP/Fizzbuzz/fizzBuzz.js
+++ Week1/Day 5/OP/Fizzbuzz/fizzBuzz.js
@@ -1,8 +1,10 @@
-for(var i = 1;i<=15;i++ ){
-    if(i%3==0){
-        console.log("Fuzz");
+for(var i = 1;i<=100;i++ ){
+    if(i%3==0 && i%5==0){
+        console.log("fuzzbuzz");
     }else if(i%5==0){
         console.log("buzz");
+    }else if(i%3==0){
+        console.log("fuzz");
     }else{
         console.log(i);
     }
